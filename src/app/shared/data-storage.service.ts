a4611e694057667644a4c283b55c0ba5f0d5be11
Diego Sanchez <diegorl84@gmail.com>
2019-07-29 10:10:34 -0500

    Use of operators: take and exhaustMap in fetch method to attached the auth token to REST service. #299

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
-import { map, tap } from "rxjs/operators";
+import { map, tap, take, exhaustMap } from "rxjs/operators";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable({ providedIn: "root" })
 export class DataStorageService {
   constructor(
     private http: HttpClient,
-    private recipesService: RecipeService
+    private recipesService: RecipeService,
+    private authService: AuthService
   ) {}
 
   storeRecipes() {
@@ -24,23 +26,33 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.http
-      .get<Recipe[]>(
-        "https://ng-complete-guide-2c30e.firebaseio.com/recipes.json"
-      )
-      .pipe(
-        map(recipes => {
-          
-          return recipes.map(recipe => {
-            return {
-              ...recipe,
-              ingredients: recipe.ingredients ? recipe.ingredients : []
-            };
-          });
-        }),
-        tap(recipes =>{
-          this.recipesService.setRecipes(recipes);
-        })
-      );
+    /**
+     * Take subscribe, take the last user and unsubscribe
+     * exhaustMap replace the first observable to the exhausted one.
+     * First the USER observable is executed but when it finished the new observable HTTP takes place
+     */
+
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap(user => {
+        return this.http.get<Recipe[]>(
+          "https://ng-complete-guide-2c30e.firebaseio.com/recipes.json",
+          {
+            params: new HttpParams().set('auth', user.token)
+          }
+        );
+      }),
+      map(recipes => {
+        return recipes.map(recipe => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : []
+          };
+        });
+      }),
+      tap(recipes => {
+        this.recipesService.setRecipes(recipes);
+      })
+    );
   }
 }
