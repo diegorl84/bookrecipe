77539f38aaa7ab502ffcf321355b2b2ac759ae75
Diego Sanchez <diegorl84@gmail.com>
2019-07-30 09:44:45 -0500

    Creation of RecipeModule and split of routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,49 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { RecipesComponent } from "./recipes/recipes.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
-import { RecipesResolverService } from "./recipes/recipes-resolver.service";
 import { AuthComponent } from "./auth/auth.component";
-import { AuthGuard } from "./auth/auth.guard";
 
 const appRoutes: Routes = [
   { path: "", redirectTo: "/auth", pathMatch: "full" },
-  {
-    path: "recipes",
-    component: RecipesComponent,
-    canActivate: [AuthGuard],
-    children: [
-      {
-        path: "",
-        component: RecipeStartComponent
-      },
-      {
-        path: "new",
-        component: RecipeEditComponent
-      },
-      {
-        path: ":index",
-        component: RecipeDetailComponent,
-        resolve: {
-          recipes: RecipesResolverService
-        }
-      },
-      {
-        path: ":index/edit",
-        component: RecipeEditComponent,
-        resolve: {
-          recipes: RecipesResolverService
-        }
-      }
-    ]
-  },
-  { path: "auth", component: AuthComponent },
   { path: "shopping-list", component: ShoppingListComponent },
-  { path: "**", component: PageNotFoundComponent }
+  { path: "auth", component: AuthComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,6 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
-import { RecipesComponent } from "./recipes/recipes.component";
-import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 import { DropdownDirective } from "./shared/dropdown.directive";
@@ -15,8 +11,6 @@ import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { RecipeService } from "./recipes/recipe.service";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { RecipesResolverService } from "./recipes/recipes-resolver.service";
 import { AuthComponent } from "./auth/auth.component";
@@ -24,21 +18,17 @@ import { LoadingSpinnerComponent } from "./shared/loading-spinner/loading-spinne
 import { AuthInterceptorService } from "./auth/auth-interceptor.service";
 import { AlertComponent } from "./shared/alert/alert.component";
 import { PlaceHolderDirective } from "./shared/placeholder/placeholder.directive";
+import { RecipesModule } from "./recipes/recipes.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
+
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
     PageNotFoundComponent,
-    RecipeStartComponent,
-    RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent,
@@ -49,7 +39,8 @@ import { PlaceHolderDirective } from "./shared/placeholder/placeholder.directive
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RecipesModule
   ],
   providers: [
     RecipeService,
